c3796a4dc23a695dd2f5c2c598c9d5dff5acf90d
serhii.shramko <shramko.web@yahoo.com>
2020-06-07 18:20:40 +0300

    implement api-server

---
+++ src/api/api.js
@@ -0,0 +1,65 @@
+'use strict';
+// Где-то в лекция говорилось, что в index.js должны быть только импорты и никакой логики, поэтому вынес отдельно.
+
+const http = require(`http`);
+const chalk = require(`chalk`);
+const fs = require(`fs`).promises;
+
+const {
+  FILE_NAME,
+  HttpCode,
+} = require('../constants');
+
+const sendResponse = (res, statusCode, message) => {
+  const template = `
+    <!Doctype html>
+      <html lang="ru">
+      <head>
+        <title>With love from Node</title>
+      </head>
+      <body>${message}</body>
+    </html>`.trim();
+
+  res.statusCode = statusCode;
+  res.writeHead(statusCode, {
+    'Content-Type': `text/html; charset=UTF-8`,
+  });
+
+  res.end(template);
+};
+
+const requestHandler = async (req, res) => {
+  const NOT_FOUND_MESSAGE = `Not found`;
+  switch (req.url) {
+    case `/`:
+      try {
+        const fileContent = await fs.readFile(FILE_NAME);
+        const mocks = JSON.parse(fileContent);
+        const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
+        sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
+      } catch (err) {
+        sendResponse(res, HttpCode.NOT_FOUND, NOT_FOUND_MESSAGE);
+      }
+
+      break;
+    default:
+      sendResponse(res, HttpCode.NOT_FOUND, NOT_FOUND_MESSAGE);
+      break;
+  }
+};
+
+const initServer = (port) => {
+  http.createServer(requestHandler)
+    .listen(port)
+    .on(`listening`, (error) => {
+      if (error) {
+        return console.error(`Ошибка при создании сервера`, error);
+      }
+
+      return console.info(chalk.green(`Ожидаю соединений на ${port}`));
+    })
+};
+
+module.exports = {
+  initServer,
+};
